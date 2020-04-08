55c22180b838300871bca04536f99a62f9b0502b
David Romanski <david_romanski@hotmail.com>
2020-04-08 16:12:55 -0400

    Update Chessboard.java

--- Eloquent JavaScript/Chapter 2/Chessboard.java
+++ Eloquent JavaScript/Chapter 2/Chessboard.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Chessboard {
 
@@ -5,12 +6,25 @@ public class Chessboard {
 		// TODO Auto-generated method stub
 		// David Romanski
 		// JavaScript Exercises
-		// 2/24/2020
+		// Created: 04/07/2020
+		// Updated: 04/08/2020
 		// Eloquent Javascript
 		// Chapter 2: Chessboard
 		// Comments: Draw a chessboard grid of size
-
-		int size = 10;
+		// Updated to allow input to determine size.
+		Scanner input = new Scanner(System.in);
+		int size;
+		
+		do {
+			System.out.print("Please enter a positive integer for the size of the requested chessboard: ");
+			while (!input.hasNextInt()) {
+				System.out.println("I'm sorry, that is not an integer.");
+				input.next();
+				System.out.print("Please enter a positive integer for the size of the requested chessboard: ");
+			} 
+			size = input.nextInt();
+		} while (size <= 0);
+		
 		for (int i=0; i < size; i++) {
 		  for (int j=0; j < size; j++)
 		    if (i%2 == 1)
@@ -22,6 +36,6 @@ public class Chessboard {
 		    else System.out.print(" ");
 		  System.out.println();
 		}
+		input.close();
 	}
-
 }
