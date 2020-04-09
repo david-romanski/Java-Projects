49442ac25e2c5144e49a73830c83f7b1e3163ad5
David Romanski <david_romanski@hotmail.com>
2020-04-09 09:33:52 -0400

    Update minimum.java

--- Eloquent JavaScript/Chapter 3/minimum.java
+++ Eloquent JavaScript/Chapter 3/minimum.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class minimum {
 
@@ -13,15 +14,36 @@ public class minimum {
 		// TODO Auto-generated method stub
 		//  David Romanski
 		//  JavaScript Exercises
-		//  4/07/2020
+		//  Created: 04/07/2020
+		//  Updated: 04/09/2020
 		//  Eloquent Javascript
 		//  Chapter 3: Minimum
 		//  Comments: Give 2 numbers displays the minimum number
+		//  Updated to include inputs and validation.
+		Scanner input = new Scanner(System.in);
 
-		int num1 = 100;
-		int num2 = 10;
+		int num1;
+		int num2;
+
+		System.out.println("Please enter two integer to determine the minimum.");
+
+		System.out.print("Please enter first integer:");
+		while(!input.hasNextInt()) {
+			System.out.println("I'm sorry, that wasn't an integer.");
+			input.next();
+			System.out.print("Please enter first integer:");				
+		} num1 = input.nextInt();
+		
+		System.out.print("Please enter second integer:");
+		while(!input.hasNextInt()) {
+			System.out.println("I'm sorry, that wasn't an integer.");
+			input.next();
+			System.out.print("Please enter second integer:");				
+		} num2 = input.nextInt();
+		
 		System.out.print("Between " + num1 + " and " + num2 + " the minimum is ");
 		System.out.print(min(num1, num2));
+		
+		input.close();
 	}
-
 }
