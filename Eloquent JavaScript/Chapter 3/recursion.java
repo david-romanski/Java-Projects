7101d1f3f078c6eb1ac74a68d8c64a5adc335a0c
David Romanski <david_romanski@hotmail.com>
2020-04-09 10:34:19 -0400

    Update recursion.java

--- Eloquent JavaScript/Chapter 3/recursion.java
+++ Eloquent JavaScript/Chapter 3/recursion.java
@@ -1,26 +1,38 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class recursion {
 
 	public static String isEven(int num) {
-		  if (num == 0)
+		  if (Math.abs(num) == 0)
 		      return "even";
-		  if (num == 1)
+		  if (Math.abs(num) == 1)
 		      return "odd";
-		  return isEven(num-2);
+		  return isEven(Math.abs(num)-2);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//  David Romanski
 		//  JavaScript Exercises
-		//  04/07/2020
+		//  Created: 04/07/2020
+		//  Updated: 04/09/2020
 		//  Eloquent Javascript
 		//  Chapter 3: Recursion
 		//  Comments: Using recursion determine if a number is odd or even
-
-
-		int num = 50;
+		//  Added input and validation.
+		
+		Scanner input = new Scanner(System.in);
+		int num;
+		
+		System.out.print("Please enter integer to determine if odd or even:");
+		while(!input.hasNextInt()) {
+			System.out.println("I'm sorry, that isn't an integer.");
+			input.next();
+			System.out.print("Please enter integer to determine if odd or even:");			
+		} num = input.nextInt();
 		System.out.print("The number " + num + " is " + isEven(num));
+		
+		input.close();
 	}
-
 }
